07ce8de87fa2bcb3750417da3a94a0ce670112ac
_iPhoenix_ <32944537+Legend-of-iPhoenix@users.noreply.github.com>
2018-04-16 19:35:42 -0400

    Left in something from a previous revision.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 eval(s=`var q=0,a=x=>{q++;document.body.innerHTML=
 s.split('\\n').map((x,j)=>x.split('').map((x,i,a)=>
 '<kbd style="color: hsl('+(360*(i+q+j))/50+',100'+
-'%,50%)">'+x+'</kbd>').join('')).join('\x3cbr />')}//
+'%,50%)">'+x+'</kbd>').join('')).join('<br />')}//
 setInterval(a,9);s='eval(s=\`'+s+'\`)'//_iPhoenix_`)
