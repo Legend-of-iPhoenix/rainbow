a681bfc526d3f432bb2fbb02d8deab1a25648e09
_iPhoenix_ <32944537+Legend-of-iPhoenix@users.noreply.github.com>
2018-04-16 19:28:26 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+eval(s=`var q=0,a=x=>{q++;document.body.innerHTML=
+s.split('\\n').map((x,j)=>x.split('').map((x,i,a)=>
+'<kbd style="color: hsl('+(360*(i+q+j))/50+',100'+
+'%,50%)">'+x+'</kbd>').join('')).join('\x3cbr />')}//
+setInterval(a,9);s='eval(s=\`'+s+'\`)'//_iPhoenix_`)
